254f13fbe7fca71d28897ced29aa344ce5d8b426
Gabriel <gs1323@ic.ac.uk>
2025-10-01 23:48:43 +0100

    reset site to just personal

--- static/toggleMenu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementsByClassName('dropdown-menu')[0];
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
-}
